31bf9a88a5d1a43e50d6cdd5ee797587aaff2696
unknown <sendevshruti@gmail.com>
2022-10-11 22:32:14 +0530

    web23for-loop

---
+++ web23for-loop/problem2.js
@@ -0,0 +1,11 @@
+
+
+let odd="";
+
+for(let n=1; n<=20; n++){
+  if(n%2==1){
+    odd+=n+" ";
+  }
+
+}
+console. log(odd);
---
+++ web23for-loop/problem3.js
@@ -0,0 +1,9 @@
+let x="";
+
+for(let n=1; n<=4; n++){
+  
+    x+=n+"-*";
+  
+
+}
+console. log(x);
